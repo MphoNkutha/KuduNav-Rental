e912d9e1b49a63a911ba1200edb01c8c5ac7d774
Mpho Nkutha <mphonkutha.02@gmail.com>
2024-10-03 09:48:27 +0200

    more-updates3

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -4,7 +4,7 @@ const reservationSchema = new mongoose.Schema({
   bikeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Bike', required: true },
   userId: { type: String, required: true },
   status: { type: String, default: 'reserved' },
-  timestamp: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Reservation = mongoose.model('Reservation', reservationSchema);
